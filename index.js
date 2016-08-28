4b275e77cb379df2321001825d5d7a9889aa1ee5
Manshant Singh Kohli <me@manshantsingh.com>
2016-08-27 21:30:57 -0700

    porting server script to connect to MySQL database

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var bodyParser=require('body-parser');
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
-var TYPES = require('tedious').TYPES;
+var mysql = require('mysql');
 
 var app = express();
 
@@ -10,20 +8,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/:tiny', function(req, res){
-	request = new Request('select dbo.geturl(@tiny)', function(err, rowCount){
-		if(err) {
-			console.log(err);
-		}
+	connection.query('select geturl(?) as complete;',req.params.tiny, function(err, rows) {
+		if(err) throw err;
+		res.redirect(rows[0].complete);
 	});
-	request.addParameter('tiny', TYPES.NVarChar, req.params.tiny);
-
-	request.on('row', function(columns){
-		if(columns[0].value){
-			res.redirect(columns[0].value);
-		}
-		else res.send("its null");
-	});
-	connection.execSql(request);
 });
 
 app.get('/', function(req, res){
@@ -33,43 +21,30 @@ app.get('/', function(req, res){
 app.post('/addurl', function(req, res){
 	var tiny = req.body.tiny;
 	var complete = req.body.complete;
+
 	if(tiny && complete){
-		request = new Request('addurl', function(err, rowCount){
-			if(err) {
-				console.log(err);
-				res.send('Something went wrong.\nMost likely smalll.xyz/'+tiny+' is already taken.');
-			}
-			else{
-				res.send('URL shortcut added.\nsmalll.xyz/'+tiny+' should redirect you to '+complete);
-			}
+		connection.query('call addurl(?, ?);',[tiny, complete], function(err, rows, fields) {
+			if(err) res.send('Something went wrong.\nMost likely smalll.xyz/'+tiny+' is already taken.');
+			else res.send('URL shortcut added.\nsmalll.xyz/'+tiny+' should redirect you to '+complete);
 		});
-		request.addParameter('tiny', TYPES.NVarChar, tiny);
-		request.addParameter('complete', TYPES.NVarChar, complete);
-		connection.callProcedure(request);
 	}
 });
 
+var connection = mysql.createConnection({
+	host		: process.env.DB_host,
+	user		: process.env.DB_user,
+	password	: process.env.DB_pass,
+	database	: process.env.DB_name
+});
 
-var config = {
-    userName: process.env.DB_user,
-    password: process.env.DB_pass,
-    server: process.env.DB_url,
-    // If you are on Microsoft Azure, you need this:
-    options: {
-    	encrypt: true,
-    	// rowCollectionOnRequestCompletion: true,
-    	database: 'smalll'
-    }
-};
-var connection = new Connection(config);
-connection.on('connect', function(err) {
-	if(err){
-		console.log(err);
-	}
-    console.log("Connected");
+connection.connect(function(err){
+	if(err) throw err;
+	console.log("Connected");
 });
 
 const port = 1337;
 app.listen(port, function(){
 	console.log('server listeneing on port ' + port);
 });
+
+// connection.end();
